9af0072d45fc9a1767a09cca543877d91496e9ac
Carlos Matos <carlos.matos@uhuu.com>
2023-09-14 11:50:50 -0300

    commit: creating the dashboard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,8 @@
 import Message from "../layout/Message";
 import { useLocation } from "react-router-dom";
+import styles from './Projects.module.css';
+import Container  from '../layout/Container';
+import LinkButton from "../layout/LinkButton";
 
 function Projects() {
 
@@ -11,9 +14,18 @@ function Projects() {
 
     return (
 
-        <div>
-            <h1>My projects</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>My projects</h1>
+            </div>
+            <div className={styles.new_project_button}>
+                <LinkButton to='/newproject' text='New Project' />
+            </div>
             {message && <Message type="success" msg={message} />}
+
+            <Container customClass="start">
+                <p>Projects</p>
+            </Container>
         </div>
     )
 }
